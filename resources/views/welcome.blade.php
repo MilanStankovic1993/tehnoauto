9ad85a96e76cf123a92f73b0cda60044c45d2419
Milan Radijator <milan.stankovic@radijator.rs>
2025-10-23 12:42:26 +0200

    * proba isporuke

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tehnoauto - Uskoro se pokreće</title>
+    <title>Tehnoauto DOO - Uskoro se pokreće</title>
     <style>
         body {
             font-family: 'Segoe UI', sans-serif;
